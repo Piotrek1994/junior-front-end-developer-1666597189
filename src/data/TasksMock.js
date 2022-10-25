b5264093b0bb80c4a06990e7da7115a17ca4b211
Piotr Prus <pprus94@gmail.com>
2022-10-25 19:22:13 +0200

    added mock data

---
+++ src/data/TasksMock.js
@@ -0,0 +1,54 @@
+const data = [
+	{
+		title: 'Application Setup',
+		status: 'closed',
+		businessContext: [],
+	},
+	{
+		title: 'Static Books List',
+		status: 'closed',
+		businessContext: [],
+	},
+	{
+		title: 'Administration Panel',
+		status: 'closed',
+		businessContext: [],
+	},
+	{
+		title: 'Connect Admin with Frontend',
+		status: 'opened',
+		businessContext: [
+			{
+				title: 'New sprint, tasks and intro information.',
+				status: 'new',
+				author: 'Olga Nelson',
+				date: '2012-04-23T18:25:43.511Z',
+				description:
+					'Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...',
+			},
+			{
+				title: 'Application has been accepted 🎉 🙌',
+				status: 'opened',
+				author: 'Kirsten Aniston ',
+				date: '2012-04-23T18:25:43.511Z',
+				description:
+					'Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...',
+			},
+			{
+				title: 'New sprint, tasks and intro information.',
+				status: 'opened',
+				author: 'Olga Nelson',
+				date: '2012-04-23T18:25:43.511Z',
+				description:
+					'Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...',
+			},
+		],
+	},
+	{
+		title: 'Book Review Feature',
+		status: 'blocked',
+		businessContext: [],
+	},
+]
+
+export default data
